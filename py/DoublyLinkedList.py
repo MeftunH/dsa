543bfd5c00932833ac536640e7b734b90ea93238
MAFTUN <maftun.hashimli@assistt.com.tr>
2024-03-22 00:01:02 +0300

    add DoublyLinkedList

---
+++ py/DoublyLinkedList.py
@@ -0,0 +1,38 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+        self.prev = None
+
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        if not self.head:
+            self.head = Node(data)
+        else:
+            new_node = Node(data)
+            cur = self.head
+            while cur.next:
+                cur = cur.next
+            cur.next = new_node
+            new_node.prev = cur
+
+    def prepend(self, data):
+        if not self.head:
+            self.head = Node(data)
+        else:
+            new_node = Node(data)
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+
+    def display(self):
+        elements = []
+        cur_node = self.head
+        while cur_node:
+            elements.append(cur_node.data)
+            cur_node = cur_node.next
+        return elements
