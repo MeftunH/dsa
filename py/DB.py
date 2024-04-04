df8a329f980f251b4b59f6da4a4d0e02381e2b16
MeftunH <maftunhashimli@gmail.com>
2024-04-04 23:58:38 +0300

    FEAT: add db algorithm

---
+++ py/DB.py
@@ -0,0 +1,38 @@
+
+interface = 'sqlite'
+db = 'db.sqlite3'
+
+
+def connect():
+    if interface == 'sqlite':
+        return sqlite3.connect(db)
+    elif interface == 'mysql':
+        return MySQLdb.connect(host='localhost', user
+
+def get_cursor():
+    return connect().cursor()
+
+def execute(query, args=None):
+    if args:
+        get_cursor().execute(query, args)
+    else:
+        get_cursor().execute(query)
+    connect().commit()
+
+def fetchone(query, args=None):
+    if args:
+        get_cursor().execute(query, args)
+    else:
+        get_cursor().execute(query)
+    return get_cursor().fetchone()
+
+def fetchall(query, args=None):
+    if args:
+        get_cursor().execute(query, args)
+    else:
+        get_cursor().execute(query)
+    return get_cursor().fetchall()
+
+def close():
+    get_cursor().close()
+    connect().close()
